dc482fbb1c196c3abccd9713764b65d30bc10d51
Herber Roman Alvarado Vasquez <roman.alvaradohn.edu@gmail.com>
2024-07-16 21:28:26 -0400

    barra de progreso mejorada

--- installer.sh
+++ installer.sh
@@ -1,40 +1,71 @@
 #!/bin/bash
 
 
+#VARIABLES GLOBALES================================
 log="$(pwd)/installer_log"
+flag="$(pwd)/stop_progress_bar"
 
-respuesta=$(whiptail --title "Instalador de apps" --menu "¿Qué desea hacer?" 20 80 6 \
-    "1" "instalar node-red" \
-    "2" "instalar grafana" \
-    "3" "desinstalar node-red" \
-    "4" "desinstalar grafana" \
-    "5" "salir" \
-   3>&1 1>&2 2>&3)
+
+menu_principal(){
+
+  while true ; do
+
+    respuesta=$(whiptail --title "Instalador de apps" --menu "¿Qué desea hacer?" 20 80 6 \
+        "1" "instalar node-red"    \
+        "2" "instalar grafana"     \
+        "3" "desinstalar node-red" \
+        "4" "desinstalar grafana"  \
+        "5" "salir"                \
+     3>&1 1>&2 2>&3)
+
+
+    if [ $? == 0 ]; then
+      case $respuesta in
+
+        "1") instalar_node_red ;;
+
+        "2") instalar_grafana ;;
+
+        "3") desinstalar_node_red ;;
+
+        "4") desinstalar_grafana ;;
+
+          *) break ;;
+      esac
+    fi
+  done
+
+  clear
+}
+
 
 
 barra_progreso(){
   {
-    i=0
-    while [ -e $log ] ; do
+    for (( i=10; i<=100; i++ )) ; do
       echo $i
+      if [ -e $flag ] ; then
+        echo 100
+        break;
+      fi
       sleep 10
-      i=$((i + 1))
     done
-    echo 100
-    sleep 1
+    sleep 3
   } | whiptail --gauge "${1}..." 6 80 0
+  rm -f $flag
 }
 
 
 proceso_finalizado(){
-  mensaje="$1"
+  msj=$1
 
- if [ -e $log ] ; then
-    mensaje="Algo salió mal, revise el archivo: $log"
+  if [ -s $log ] ; then
+    msj="Algo salio mal, revise el archivo: ${log}"
+  else
+    rm -f $log
   fi
 
-  whiptail --title "proceso finalizado" --msgbox "$mensaje" 10 80
-  clear
+  whiptail --title "proceso finalizado" --msgbox "$msj" 10 80
 }
 
 
@@ -44,17 +75,15 @@ proceso_finalizado(){
 
 
 instalar_node_red(){
-  proceso="instalando node-red"
-  $((npm install -g --unsafe-perm node-red >$log 2>$log) && rm -fr $log) &
-  barra_progreso "$proceso"
-  proceso_finalizado "$proceso"
+  npm install -g --unsafe-perm node-red >/dev/null 2>$log && touch $flag &
+  barra_progreso "Instalando node-red, por favor espere..."
+  proceso_finalizado "Instalacion node-red completada"
 }
 
 desinstalar_node_red(){
-  proceso="desinstalando node-red"
-  $((npm uninstall -g node-red >$log 2>$log) && rm -fr $log) &
-  barra_progreso "$proceso"
-  proceso_finalizado "$proceso"
+  npm uninstall -g node-red >/dev/null 2>$log && touch $flag &
+  barra_progreso "desinstalando node-red, por favor espere"
+  proceso_finalizado "Desinstalacion de node-red completada"
 }
 
 
@@ -62,34 +91,17 @@ desinstalar_node_red(){
 
 
 instalar_grafana(){
-  proceso="instalando grafana"
-  $((yum install -y https://dl.grafana.com/enterprise/release/grafana-enterprise-11.1.0-1.x86_64.rpm >$log 2>$log) && rm -fr $log) &
-  barra_progreso "$proceso"
-  proceso_finalizado "$proceso"
+  dnf install -y https://dl.grafana.com/enterprise/release/grafana-enterprise-11.1.0-1.x86_64.rpm >/dev/null 2>$log && touch $flag &
+  barra_progreso "Instalando grafana, por favor espere..."
+  proceso_finalizado "Instalacion de grafana completada"
 }
 
 desinstalar_grafana(){
-  proceso="desinstalando grafana"
-  $((dnf remove grafana -y >$log 2>$log) && rm -fr $log) &
-  barra_progreso "$proceso"
-  proceso_finalizado "$proceso"
+  dnf remove grafana -y >/dev/null 2>$log && touch $flag &
+  barra_progreso "Desinstalando grafana, por favor espere..."
+  proceso_finalizado "Desinstalacion de grafana completada"
 }
 
 
-if [ $? == 0 ]; then
-  case $respuesta in
 
-    "1") instalar_node_red ;;
-
-    "2") instalar_grafana ;;
-
-    "3") desinstalar_node_red ;;
-
-    "4") desinstalar_grafana ;;
-
-    "5") clear ; exit ;;
-
-      *) clear; exit ;;
-
-    esac
-fi
+menu_principal
